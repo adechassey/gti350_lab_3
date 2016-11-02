542221502f916c2f3fb9870fc546dfd13c96de84
Antoine de Chassey <antoinedechassey@gmail.com>
2016-11-02 12:24:40 -0500

    FindEvent search bar

--- app/src/main/java/gti350/lab/meetsports/FindEventActivity.java
+++ app/src/main/java/gti350/lab/meetsports/FindEventActivity.java
@@ -1,17 +1,69 @@
 package gti350.lab.meetsports;
 
+import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 /**
- * Created by VMabille on 31/10/2016.
+ * Created by Antoine on 02/10/2016.
+ *
+ * A big thanks to this tutorial:
+ * https://androidhub.intel.com/en/posts/nglauber/Android_Search.html
  */
 
-public class FindEventActivity extends FragmentActivity {
+public class FindEventActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_event);
+        TextView txt = (TextView)findViewById(R.id.textView);
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            txt.setText("Searching by: "+ query);
+
+        } else if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+            String uri = intent.getDataString();
+            txt.setText("Suggestion: "+ uri);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(
+                new ComponentName(this, FindEventActivity.class)));
+        searchView.setIconifiedByDefault(false);
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        // User pressed the search button
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        // User changed the text
+        return false;
     }
 }
